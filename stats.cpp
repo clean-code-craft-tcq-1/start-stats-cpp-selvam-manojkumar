c38176d560d25fe4219d1a986809e4f60cfe033d
selvam-manojkumar <77610809+selvam-manojkumar@users.noreply.github.com>
2021-01-22 08:57:47 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,7 +1,7 @@
 #include "stats.h"
 #include <cmath>
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+Stats Statistics::ComputeStatistics(const std::vector<float>& ) {
     //Implement statistics here
     Stats temp={NAN, NAN, NAN};
     return temp;
