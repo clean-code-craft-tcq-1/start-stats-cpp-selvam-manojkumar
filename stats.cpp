bfb5422cb90cc8f19092041fa2b7a45e0ea49907
selvam-manojkumar <77610809+selvam-manojkumar@users.noreply.github.com>
2021-01-25 16:58:09 +0530

    Update stats.cpp
    updated implementation for ComputeStatistics.

--- stats.cpp
+++ stats.cpp
@@ -1,8 +1,32 @@
 #include "stats.h"
 #include <cmath>
 
-Stats Statistics::ComputeStatistics(const std::vector<float>& ) {
+Stats Statistics::ComputeStatistics(const std::vector<float>&input ) {
     //Implement statistics here
+    double sum=0;
+    static float prev = 0;
     Stats temp={NAN, NAN, NAN};
+    if(input.empty())
+    {
+       return temp; 
+    }
+    temp.min = input[0];
+    temp.max = input[0];
+    prev = input[0];
+    for(n:input)
+    {
+        if(n>prev)
+        {
+            temp.max = n;
+        }
+        else
+        {
+            temp.min = n;
+        }
+        sum += n;
+        prev = n
+    }
+    temp.average = sum/input.size;
+    
     return temp;
 }
