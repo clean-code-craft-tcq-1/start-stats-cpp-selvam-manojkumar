4a77114ece29df5ff0ca817f4073eba3e96962a4
selvam-manojkumar <77610809+selvam-manojkumar@users.noreply.github.com>
2021-01-27 17:06:39 +0530

    Update stats.h
    updated implementation for testcase3.

--- stats.h
+++ stats.h
@@ -7,3 +7,33 @@ struct Stats{
 namespace Statistics {
     Stats ComputeStatistics(const std::vector<float>& );
 }
+class IAlerter {
+public:
+	virtual ~IAlerter() {};
+	virtual void setstatus(bool flag) = 0;
+};
+
+class EmailAlert : public IAlerter
+{
+public:
+	bool emailSent;
+	EmailAlert(); 
+	virtual void setstatus(bool flag);
+};
+
+class LEDAlert : public IAlerter
+{
+public:
+	bool ledGlows;
+	LEDAlert();
+	virtual void setstatus(bool flag);
+};
+
+class StatsAlerter {
+	
+public:
+	float maxThreshold;
+	const std::vector<IAlerter*> objList;
+	StatsAlerter(float maxThreshold, const std::vector<IAlerter*>&list);
+	void checkAndAlert(const std::vector<float>&);
+};
