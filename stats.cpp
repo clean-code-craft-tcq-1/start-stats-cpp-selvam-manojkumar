a4b827b76bf66f292c4c73f4243fd41fa8480dd1
selvam-manojkumar <77610809+selvam-manojkumar@users.noreply.github.com>
2021-01-27 17:46:23 +0530

    Update stats.cpp
    changed the logic for case1

--- stats.cpp
+++ stats.cpp
@@ -3,28 +3,27 @@
 
 Stats Statistics::ComputeStatistics(const std::vector<float>&input ) {
     //Implement statistics here
-    double sum=0;
+    float sum=0;
     static float prev = 0;
-    Stats temp={NAN, NAN, NAN};
+	Stats temp = {NAN, NAN, NAN};
     if(input.empty())
     {
        return temp; 
     }
     temp.min = input[0];
     temp.max = input[0];
-    prev = input[0];
     for(auto n:input)
     {
-        if(n>prev)
+        if(n>temp.max)
         {
             temp.max = n;
         }
-        else
+        if( n<temp.min)
         {
             temp.min = n;
         }
         sum += n;
-        prev = n;
+
     }
     temp.average = sum/input.size();
     
